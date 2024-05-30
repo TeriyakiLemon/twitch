ffd6dab830c6204297d82b02bf9a6bab711b7222
Tianchen zhang <jasontc196@gmail.com>
2024-05-30 13:34:32 -0400

    update GameController ItemController ItemEntity ItemService

--- src/main/java/com/laioffer/twitch/TwitchApplication.java
+++ src/main/java/com/laioffer/twitch/TwitchApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+
 public class TwitchApplication {
 
 	public static void main(String[] args) {
